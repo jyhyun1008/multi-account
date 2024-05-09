c1edbc914ce4ab415c608b90212aa2c94ce8223c
HiYUNo <jayhyun1008@gmail.com>
2024-05-09 15:03:58 +0900

    ?

--- js/manual.js
+++ js/manual.js
@@ -1,10 +1,9 @@
 if (accounts.length > 0 && mode == 'manual') {
 
-    document.querySelector('#post-box').innerHTML += '<div id="select-box"><select name="account" id="account"></div>'
+    document.querySelector('#post-box').innerHTML += '<div id="select-box"><select id="select-input" name="account" id="account"></select></div>'
 
     for (var i=0; i<accounts.length; i++) {
-        document.querySelector('#select-box').innerHTML += '<option value="'+i+'">@'+accounts[i].username+'@'+accounts[i].host+'</option>'
+        document.querySelector('#select-input').innerHTML += '<option value="'+i+'">@'+accounts[i].username+'@'+accounts[i].host+'</option>'
     }
-    document.querySelector('#select-box').innerHTML += '</select>'
 
 }
