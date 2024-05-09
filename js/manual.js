8d6b8636229b8d1aa17a0c851c7943058a1d949b
HiYUNo <jayhyun1008@gmail.com>
2024-05-10 07:48:37 +0900

    버튼 disable

--- js/manual.js
+++ js/manual.js
@@ -72,6 +72,7 @@ async function post(accountIndex, text) {
     getTimeLine()
 
     document.querySelector(`#post-input`).value = ''
+    document.querySelector('#gpt-button').disabled = true
     document.querySelector('#post-button').disabled = true
 
 }
