a55a766746a7d2b327fd0e38e1007f91584fc37d
HiYUNo <jayhyun1008@gmail.com>
2024-05-10 07:16:10 +0900

    오타

--- js/gpt.js
+++ js/gpt.js
@@ -12,6 +12,6 @@ if (page == 'gpt') {
         localStorage.setItem("gptToken", token)
     }
 
-    document.querySelector('#post-box').innerHTML = '<div id="token-label">OpenAI 액세스 토큰:</div><input id="token-input" oninput="changeTokenDisabled(this)">><button id="token-button" disabled="true" onclick="addGptToken(document.querySelector(`#token-input`).value)">저장</button>'
+    document.querySelector('#post-box').innerHTML = '<div id="token-label">OpenAI 액세스 토큰:</div><input id="token-input" oninput="changeTokenDisabled(this)"><button id="token-button" disabled="true" onclick="addGptToken(document.querySelector(`#token-input`).value)">저장</button>'
 
 }
