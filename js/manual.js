06dbc16c9405aa3ad4a1e0bcf46565899ac29460
HiYUNo <jayhyun1008@gmail.com>
2024-05-09 16:24:52 +0900

    멩

--- js/manual.js
+++ js/manual.js
@@ -46,7 +46,7 @@ if (accounts.length > 0 && mode == 'manual') {
         document.querySelector('#post-box').innerHTML += '<div id="select-box"><select id="select-input" name="account" id="account"></select></div>'
 
         for (var i=0; i<accounts.length; i++) {
-            document.querySelector('#select-input').innerHTML += '<option value="'+i+'">@'+accounts[i].username+'@'+accounts[i].host+'</option>'
+            document.querySelector('#select-input').innerHTML += '<option value="'+i+'">'+accounts[i].role+' (@'+accounts[i].username+'@'+accounts[i].host+')</option>'
         }
 
     }
